185f94607ab62938c7d32a7232d775d9eb4d9310
matumba 125 <nbenza5@gmail.com>
2021-09-08 17:47:40 +0300

    fixed npm version in package.json

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
@@ -20,4 +19,3 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
